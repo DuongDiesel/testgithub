9ec652f7b59d7d491a00ad26dc0f62d1e07c5243
Duong Nguyen <duongbgvip1@gmail.com>
2020-07-05 19:38:02 +0900

    add body parser
    add body parser

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const bodyParser = require('body-parser');
 
 //const config = require('./config');
 const line = require('@line/bot-sdk');
@@ -19,7 +20,7 @@ const client = new line.Client(config);
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-
+  .use(bodyParser.json())
   
 
   .post('/webhook/', (req, res) => {
