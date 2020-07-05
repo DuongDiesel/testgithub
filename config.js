fe53f237709ed41eca2189d3d0b452137fa59275
Duong Nguyen <duongbgvip1@gmail.com>
2020-07-05 18:35:42 +0900

    add index

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    linechannelId: process.env.LINE_CHANNEL_ID,
+    linechannelSecret: process.env.LINE_CHANNEL_SECRET,
+    linechannelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+    // PG_CONFIG: {
+    //     user: process.env.PG_CONFIG_USER,
+    //     database: process.env.PG_CONFIG_DATABASE,
+    //     password: process.env.PG_CONFIG_PASSWORD,
+    //     host: process.env.PG_CONFIG_HOST,
+    //     port: 5432,
+    //     max: 10,
+    //     idleTimeoutMillis: 30000,        
+    //     ssl:{rejectUnauthorized : false} // dong nay de sua loi  self signed certificate     https://github.com/typeorm/typeorm/issues/278  
+       
+    // }
+
+};
--- index.js
+++ index.js
@@ -2,8 +2,28 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const middleware = require('@line/bot-sdk').middleware
+const JSONParseError = require('@line/bot-sdk').JSONParseError
+const SignatureValidationFailed = require('@line/bot-sdk').SignatureValidationFailed
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(middleware(config))
+  .post('/webhook', (req, res) => {
+    res.json(req.body.events) // req.body will be webhook event object
+  })
+
+  .use((err, req, res, next) => {
+    if (err instanceof SignatureValidationFailed) {
+      res.status(401).send(err.signature)
+      return
+    } else if (err instanceof JSONParseError) {
+      res.status(400).send(err.raw)
+      return
+    }
+    next(err) // will throw default 500
+  })
+
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
